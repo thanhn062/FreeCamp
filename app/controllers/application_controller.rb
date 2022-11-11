0ae71d27a6069d993e6dd38a631c186cf7140c6b
thanh <510thanh.ngo@gmail.com>
2022-11-10 22:15:47 -0800

    added redirect to project after sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
     before_action :authenticate_user!
+
+    protected
+    def after_sign_in_path_for(resource)
+      "/projects"
+    end
 end
