19a9ecfe7c8265c50570c74ea7c680c244509e7c
thanh <510thanh.ngo@gmail.com>
2022-11-11 23:39:40 -0800

    added name into registration form and database

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,15 @@
 class ApplicationController < ActionController::Base
     before_action :authenticate_user!
+    before_action :configure_permitted_parameters, if: :devise_controller?
 
     protected
     def after_sign_in_path_for(resource)
       "/projects"
+    end
+
+    def configure_permitted_parameters
+      attributes = [:name, :email, :password, :password_confirmation]
+      devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
+      devise_parameter_sanitizer.permit(:account_update, keys: attributes)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -19,6 +19,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_07_025221) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "name", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
