4b1101d2befdb8be941dda85fed4907b94a3ac36
thanh <510thanh.ngo@gmail.com>
2022-11-15 23:14:48 -0800

    add admin column to permit field

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
     end
 
     def configure_permitted_parameters
-      attributes = [:name, :email, :password, :password_confirmation]
+      attributes = [:name, :admin, :email, :password, :password_confirmation]
       devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
       devise_parameter_sanitizer.permit(:account_update, keys: attributes)
     end
