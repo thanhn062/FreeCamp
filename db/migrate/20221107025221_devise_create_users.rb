09a41e7053943982accf7eb4ab773655c1bbd223
thanh <510thanh.ngo@gmail.com>
2022-11-15 20:32:50 -0800

    add admin column

--- db/migrate/20221107025221_devise_create_users.rb
+++ db/migrate/20221107025221_devise_create_users.rb
@@ -4,6 +4,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       t.string :name,               null: false
+      t.boolean :admin,             null: false, default: false
       
       ## Database authenticatable
       t.string :email,              null: false, default: ""
