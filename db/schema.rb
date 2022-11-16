f9f111aeb4fa14491343d05330f89feade8ae1db
thanh <510thanh.ngo@gmail.com>
2022-11-15 21:45:11 -0800

    add admin column to db

--- db/schema.rb
+++ db/schema.rb
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_07_025221) do
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
+    t.boolean "admin", default: false, null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
