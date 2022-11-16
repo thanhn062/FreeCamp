512c37e69657e7aa34b2488dea548ea2f11f77de
thanh <510thanh.ngo@gmail.com>
2022-11-15 20:06:15 -0800

    added devise user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'landing_page/index'
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
   resources :projects
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   #root to: redirect("/projects")
